981bb33d14fb0013d5b9da40150f662916cf4159
Taha Elangovan <118097151+tahakelan@users.noreply.github.com>
2024-05-07 12:50:53 +0530

    Changes

--- SQL/3. Aggregation/4. Basic Join/01. Population Census.sql
+++
@@ -1,9 +0,0 @@
--- https://www.hackerrank.com/challenges/asian-population/problem?isFullScreen=true
--- # Difficulty : Easy
-
-select
-sum(city.population)
-from city
-inner join country on city.countrycode = country.code
-where country.continent = 'Asia'
-;
--- SQL/3. Aggregation/4. Basic Join/02. African Cities.sql
+++
@@ -1,9 +0,0 @@
--- https://www.hackerrank.com/challenges/african-cities/problem?isFullScreen=true
--- # Difficulty : Easy
-
-select
-city.name
-from city
-inner join country on city.countrycode = country.code
-where country.continent = 'Africa'
-;
