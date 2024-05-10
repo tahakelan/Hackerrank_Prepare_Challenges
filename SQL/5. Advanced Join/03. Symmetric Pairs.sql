a37aef27495166ec918c0811ed9a682b0e478bb3
Taha Elangovan <118097151+tahakelan@users.noreply.github.com>
2024-05-11 00:11:10 +0530

    Update 03. Symmetric Pairs.sql

--- SQL/5. Advanced Join/03. Symmetric Pairs.sql
+++ SQL/5. Advanced Join/03. Symmetric Pairs.sql
@@ -1,5 +1,5 @@
 -- https://www.hackerrank.com/challenges/symmetric-pairs/problem?isFullScreen=true
--- # Difficulty : Hard
+-- # Difficulty : Medium
 
 select a.x, a.y
     from functions a join functions b on a.x=b.y and b.x=a.y
