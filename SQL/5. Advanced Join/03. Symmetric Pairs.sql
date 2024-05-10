47aa77084752d26c137cd3236d44eff2e80f8c9a
Taha Elangovan <118097151+tahakelan@users.noreply.github.com>
2024-05-11 00:09:35 +0530

    Create 03. Symmetric Pairs.sql

---
+++ SQL/5. Advanced Join/03. Symmetric Pairs.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/symmetric-pairs/problem?isFullScreen=true
+-- # Difficulty : Hard
+
+select a.x, a.y
+    from functions a join functions b on a.x=b.y and b.x=a.y
+    group by a.x, a.y
+    having count(a.x)>1 or a.x<a.y
+order by a.x
+;
