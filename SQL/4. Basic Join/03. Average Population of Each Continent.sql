c26a2771e2f60c077e144e567a6dc0b03f25a348
Taha Elangovan <118097151+tahakelan@users.noreply.github.com>
2024-05-10 22:43:54 +0530

    Update 03. Average Population of Each Continent.sql

--- SQL/4. Basic Join/03. Average Population of Each Continent.sql
+++ SQL/4. Basic Join/03. Average Population of Each Continent.sql
@@ -2,7 +2,8 @@
 -- # Difficulty : Easy
 
 select
-country.continent, floor(avg(city.population)) as Average_City_Population
+  country.continent
+  , floor(avg(city.population)) as Average_City_Population
 from city
 inner join country on city.countrycode = country.code
 group by country.continent
