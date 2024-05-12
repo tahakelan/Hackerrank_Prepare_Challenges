b2b892e7c90d16bbf55fea2f3ee095b6b05f6956
Taha Elangovan <118097151+tahakelan@users.noreply.github.com>
2024-05-13 01:27:50 +0530

    Update 05. 15 Days of Learning SQL.sql

--- SQL/5. Advanced Join/05. 15 Days of Learning SQL.sql
+++ SQL/5. Advanced Join/05. 15 Days of Learning SQL.sql
@@ -3,16 +3,18 @@
 
 select 
     submission_date
-    , (select count(distinct hacker_id)  from submissions s2  where s2.submission_date = s1.submission_date and    
-                (select count(distinct s3.submission_date) from submissions s3 
-                    where s3.hacker_id = s2.hacker_id 
-                    and s3.submission_date < s1.submission_date) = datediff(s1.submission_date , '2016-03-01')
+    , (select count(distinct hacker_id)  from submissions s2  
+            where s2.submission_date = s1.submission_date and    
+            (select count(distinct s3.submission_date) from submissions s3 
+                where s3.hacker_id = s2.hacker_id 
+                and s3.submission_date < s1.submission_date) = datediff(s1.submission_date , '2016-03-01')
       )
      , (select hacker_id from submissions s2 where s2.submission_date = s1.submission_date 
+            group by hacker_id 
+            order by count(submission_id) desc, hacker_id limit 1) 
+    as tmp
+    , (select name from hackers where hacker_id = tmp)
 
-group by hacker_id 
-order by count(submission_id) desc, hacker_id limit 1) as tmp,
-(select name from hackers where hacker_id = tmp)
 from
 (select distinct submission_date from submissions) s1
 group by submission_date
