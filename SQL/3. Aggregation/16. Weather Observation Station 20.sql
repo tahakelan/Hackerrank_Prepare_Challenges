2a72252a5b31c1ed4a95be9cbce3362816d845cd
Taha Elangovan <118097151+tahakelan@users.noreply.github.com>
2025-12-27 23:23:32 +0530

    Update 16. Weather Observation Station 20.sql

--- SQL/3. Aggregation/16. Weather Observation Station 20.sql
+++ SQL/3. Aggregation/16. Weather Observation Station 20.sql
@@ -11,4 +11,22 @@ WITH i AS (
 
 SELECT ROUND(AVG(i.LAT_N),4) AS Median_Northern_Latitudes
 FROM i
+
 WHERE i.row_no IN (FLOOR(@rowindex/2),CEIL(@rowindex/2));
+
+
+-- Solution 2
+
+select round(lat_n,4)
+from 
+(
+    select 
+        lat_n,
+        row_number() over (order by lat_n) as rn,
+        count(*) over () as cnt    
+    from station
+) as a
+where rn in (ceil((cnt+1)/2),floor((cnt+1)/2))
+
+
+;
